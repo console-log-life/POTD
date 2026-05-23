08c6144e170400e6f8551294420381919cb55ef6
Apeksha Jain <morenaapekshajain2209@gmail.com>
2026-05-23 22:52:18 +0530

    Create 23-05-2026.cpp

---
+++ LEETCODE/MAY/23-05-2026.cpp
@@ -0,0 +1,17 @@
+// 1752. Check if Array Is Sorted and Rotated
+
+
+
+class Solution {
+public:
+    bool check(vector<int>& nums) {
+        bool fault = 0;
+        int n = nums.size();
+
+        for (int i = 0; i < n; i++)
+            if (nums[i] > nums[(i + 1) % n] && exchange(fault, 1))
+                return 0;
+
+        return 1;
+    }
+};
