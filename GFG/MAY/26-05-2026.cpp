67b6c7ab8dd9934f50e5da81ae3078e2209c7f66
Apeksha Jain <morenaapekshajain2209@gmail.com>
2026-05-26 21:19:42 +0530

    Implement minToggle function to partition array

---
+++ GFG/MAY/26-05-2026.cpp
@@ -0,0 +1,22 @@
+// Minimum Toggle to Partition
+
+
+
+class Solution {
+  public:
+    virtual int minToggle(vector<int>& arr){
+      int zero=0;
+      for(int x: arr) if(!x) zero++;
+      
+      int one=0;
+      int ans=zero;
+      
+      for(int x: arr){
+        if(x==1) one++;
+        else zero--;
+        
+        ans=min(ans, zero+one);
+      }
+      return ans;
+    }
+};
