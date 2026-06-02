59c33f3275074bf602bd76bc163d6ee939dc51ae
Apeksha Jain <morenaapekshajain2209@gmail.com>
2026-06-02 23:12:03 +0530

    Create 01-06-2026.cpp

---
+++ LEETCODE/JUNE/01-06-2026.cpp
@@ -0,0 +1,18 @@
+// 2144. Minimum Cost of Buying Candies With Discount
+
+
+
+class Solution {
+public:
+    int minimumCost(vector<int>& cost) {
+        sort(cost.begin(), cost.end(), greater<int>());
+        int res = 0;
+        int n = cost.size();
+        for (int i = 0; i < n; ++i) {
+            if (i % 3 != 2) {
+                res += cost[i];
+            }
+        }
+        return res;
+    }
+};
