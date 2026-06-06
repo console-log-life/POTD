dfc342b7462050c77743ec911cda05ea2de24e67
Apeksha Jain <morenaapekshajain2209@gmail.com>
2026-06-06 23:37:08 +0530

    Update 06-06-2026.cpp

--- GFG/JUNE/06-06-2026.cpp
+++ GFG/JUNE/06-06-2026.cpp
@@ -1,18 +1,32 @@
-// 2574. Left and Right Sum Differences
+// Non-Attacking Black and White Knights
+
 
 
 class Solution {
-public:
-    vector<int> leftRightDifference(vector<int>& nums) {
-        const int n=nums.size();
-        vector<int> ans(n);
-        int lsum=0, rsum=accumulate(nums.begin(), nums.end(), 0);
+  public:
+ 
+    int numOfWays(int n, int m) {
+        auto f = [&](int i, int j)->bool{
+          return i>=0 && j>=0 && i<n && j<m;  
+        };
+        vector<int> d = {-2,-1,1,2};
+        int ans = 0;
+        int total = n*m;
         for(int i=0; i<n; i++){
-            const int x=nums[i];
-            rsum-=x;
-            ans[i]=(rsum>=lsum)?rsum-lsum:lsum-rsum;
-            lsum+=x;
+            for(int j=0; j<m; j++){
+                int cnt = 0;
+                cnt += f(i-2,j-1);
+                cnt += f(i-2,j+1);
+                cnt += f(i+2,j-1);
+                cnt += f(i+2,j+1);
+                cnt += f(i-1,j-2);
+                cnt += f(i-1,j+2);
+                cnt += f(i+1,j-2);
+                cnt += f(i+1,j+2);
+                ans += (total - cnt - 1);
+            }
         }
+        
         return ans;
     }
 };
