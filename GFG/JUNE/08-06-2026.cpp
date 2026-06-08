833750616294c2a4010a83db239d5493cb1b6971
Apeksha Jain <morenaapekshajain2209@gmail.com>
2026-06-08 12:08:02 +0530

    Create 08-06-2026.cpp

---
+++ GFG/JUNE/08-06-2026.cpp
@@ -0,0 +1,57 @@
+// Delete Nodes with Greater on Right
+
+
+
+/* Structure of a Linked List node
+class Node {
+  public:
+    int data;
+    Node* next;
+
+    Node(int x) {
+        data = x;
+        next = nullptr;
+    }
+};
+*/
+class Solution {
+public:
+
+    Node* reverse(Node* head) {
+        Node* prev = NULL;
+        Node* curr = head;
+
+        while (curr) {
+            Node* next = curr->next;
+            curr->next = prev;
+            prev = curr;
+            curr = next;
+        }
+
+        return prev;
+    }
+
+    Node* compute(Node* head) {
+
+        if (!head || !head->next)
+            return head;
+
+        head = reverse(head);
+
+        int mx = head->data;
+        Node* curr = head;
+
+        while (curr && curr->next) {
+
+            if (curr->next->data < mx) {
+                curr->next = curr->next->next;
+            }
+            else {
+                curr = curr->next;
+                mx = curr->data;
+            }
+        }
+
+        return reverse(head);
+    }
+};
