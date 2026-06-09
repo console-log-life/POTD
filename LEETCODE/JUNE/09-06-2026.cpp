3dd679ba39b88cb6b1bfaf03e76775c8ddb67422
Apeksha Jain <morenaapekshajain2209@gmail.com>
2026-06-09 19:44:49 +0530

    Create 09-06-2026.cpp

---
+++ LEETCODE/JUNE/09-06-2026.cpp
@@ -0,0 +1,17 @@
+// 3689. Maximum Total Subarray Value I
+
+
+
+class Solution {
+public:
+    long long maxTotalValue(vector<int>& A, int k) {
+        int gMin = A.front(), gMax = A.front();
+
+        for (auto& n : A) {
+            gMin = min(gMin, n);
+            gMax = max(gMax, n);
+        }
+        
+        return (long long)(gMax - gMin) * k;
+    }
+};
